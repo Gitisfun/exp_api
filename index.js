55386aca569258de67a5ceb6cf172a1d8864f4cc
Gitisfun <christianvandenputtelaar@gmail.com>
2022-03-01 22:31:39 +0100

    Format code and add maps

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import 'dotenv/config'
+import "dotenv/config";
 import Express from "express";
 import http from "http";
 import cors from "cors";
-import ApiError from './errors/ApiError.js';
-import errorHandler from './errors/ErrorHandler.js';
+import ApiError from "./errors/ApiError.js";
+import errorHandler from "./errors/ErrorHandler.js";
 
 const app = Express();
 const server = http.createServer(app);
@@ -14,12 +14,11 @@ app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  console.log("Test voor api");
   res.send("Welcome to the API!!! (v2.0)");
 });
 
 app.use((req, res, next) => {
-    next(ApiError.notFound("Route not found"));
+  next(ApiError.notFound("Route not found"));
 });
 
 app.use(errorHandler);
