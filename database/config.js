8dac61d3c0b228ba4afc5d619a37f15f13bc09f3
Gitisfun <christianvandenputtelaar@gmail.com>
2022-03-01 22:33:27 +0100

    Add config file

---
+++ database/config.js
@@ -0,0 +1,18 @@
+import mysql from "mysql"
+
+const config = {
+  host: process.env.CO_HOST,
+  user: process.env.CO_USER,
+  password: process.env.CO_PASSWORD,
+  database: process.env.CO_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  supportBigNumbers: true,
+  bigNumberStrings: true,
+  timezone: process.env.CO_TIMEZONE
+};
+
+const pool = mysql.createPool(config);
+
+export default pool
