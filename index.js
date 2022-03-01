e9c23fa116279eb1918304c41008b9834c8b61f9
Gitisfun <christianvandenputtelaar@gmail.com>
2022-03-01 21:53:55 +0100

    Project setup

---
+++ index.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import Express from "express";
+import http from "http";
+import cors from "cors";
+
+const app = Express();
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!!!");
+});
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
